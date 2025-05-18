31010b154102b5362088985df900a47d526a4bbb
sergio <sergio.alvarez@evalueserve.com>
2025-05-18 19:41:09 -0400

    feat: initial confg project

---
+++ components/navbar.tsx
@@ -0,0 +1,75 @@
+import {
+  Navbar as HeroUINavbar,
+  NavbarContent,
+  NavbarMenu,
+  NavbarMenuToggle,
+  NavbarItem,
+  NavbarMenuItem,
+  NavbarBrand,
+} from "@heroui/navbar";
+import { Link } from "@heroui/link";
+import NextLink from "next/link";
+
+import { siteConfig } from "@/config/site";
+import { Logo } from "@/components/icons";
+
+export const Navbar = () => {
+  return (
+    <HeroUINavbar className="bg-yellow-400" maxWidth="xl" position="sticky">
+      <NavbarContent className=" sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink
+            className="flex justify-start items-center gap-1 text-black"
+            href="/"
+          >
+            <Logo />
+            <p className="font-bold text-inherit ">Blazer</p>
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="sm:basis-full" justify="center">
+        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+          {siteConfig.navItems.map((item) => (
+            <NavbarItem key={item.href}>
+              <NextLink
+                className={
+                  "data-[active=true]:text-primary data-[active=true]:font-medium font-extrabold text-black"
+                }
+                color="foreground"
+                href={item.href}
+              >
+                {item.label}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </ul>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {siteConfig.navMenuItems.map((item, index) => (
+            <NavbarMenuItem key={`${item}-${index}`}>
+              <Link
+                color={
+                  index === 2
+                    ? "primary"
+                    : index === siteConfig.navMenuItems.length - 1
+                      ? "danger"
+                      : "foreground"
+                }
+                href="#"
+                size="lg"
+              >
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
+    </HeroUINavbar>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,50 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + HeroUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Nosotros",
+      href: "/Nosotros",
+    },
+    {
+      label: "Servicios",
+      href: "/servicios",
+    },
+    {
+      label: "Contacto",
+      href: "/contacto",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Inicio",
+      href: "/",
+    },
+    {
+      label: "Nosotros",
+
+      href: "/Nosotros",
+    },
+    {
+      label: "Servicios",
+      href: "/servicios",
+    },
+    {
+      label: "Contacto",
+      href: "/contacto",
+    },
+  ],
+  links: {
+    github: "https://github.com/heroui-inc/heroui",
+    twitter: "https://twitter.com/hero_ui",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/9b6yyZKmH4",
+    sponsor: "https://patreon.com/jrgarciadev",
+  },
+};
