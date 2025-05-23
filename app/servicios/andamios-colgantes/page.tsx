0b7ba9c099827da2c15a57c816572efdd773ac16
sergio <sergio.alvarez@evalueserve.com>
2025-05-22 22:03:32 -0400

    feat: add andamios-colgantes info page

--- app/servicios/andamios-colgantes/page.tsx
+++ app/servicios/andamios-colgantes/page.tsx
@@ -1,9 +1,67 @@
-import { title } from "@/components/primitives";
+import Image from "next/image";
+
+import HeaderPageTitle from "@/components/headerPageTitle";
+import ListPage from "@/components/listPage";
+import SmallCard from "@/components/smallCard";
+
+const caracteristicas = [
+  "Fácil instalación y desmontaje.",
+  "Acceso seguro a áreas de difícil acceso.",
+  "Diseño modular y versátil.",
+  "Capacidad de carga adecuada para trabajos pesados.",
+  "Estabilidad y seguridad garantizadas.",
+];
 
 export default function AndamiosColgantesPage() {
   return (
-    <div>
-      <h1 className={title()}>AndamiosColgantesPage</h1>
+    <div className="w-full flex items-start text-start px-4 py-10 flex-col gap-8">
+      <HeaderPageTitle pageTitle="Andamios Colgantes" />
+
+      <p className="text-2xl font-extrabold leading-snug">
+        Andamios colgantes para trabajos en altura.
+      </p>
+      <div className="flex gap-4 justify-center w-full">
+        <Image
+          alt="Andamio"
+          className="w-full object-cover opacity-70 rounded-xl"
+          height={150}
+          src="/andamio-colgante.jpg"
+          width={150}
+        />
+        <Image
+          alt="Andamio"
+          className="w-full  object-cover opacity-70 rounded-xl"
+          height={150}
+          src="/andamio-colgante.jpg"
+          width={150}
+        />
+      </div>
+      <div className="text-start flex flex-col gap-4 text-lg font-bold">
+        <p className="text-md font-light text-justify">
+          Solución versátil y eficiente para trabajos en altura. Acceso seguro y
+          cómodo a áreas de difícil acceso, como fachadas de edificios, techos y
+          estructuras industriales. Ideales para trabajos de mantenimiento,
+          limpieza, pintura y construcción.
+        </p>
+      </div>
+
+      <div className="w-full bg-yellow-500 py-8">
+        <div className="flex flex-col gap-4 text-white">
+          <h2 className="text-4xl font-extrabold leading-snug text-center my-6">
+            Características
+          </h2>
+          <div className="w-full flex flex-col gap-4">
+            <ListPage caracteristicas={caracteristicas} />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full text-start flex flex-col gap-4 text-lg font-bold">
+        <h2 className="text-4xl font-extrabold leading-snug text-center my-6">
+          Calidad Garantizada y Seguridad Comprobada
+        </h2>
+        <SmallCard />
+      </div>
     </div>
   );
 }
---
+++ components/listPage.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export default function ListPage({
+  caracteristicas,
+  imgUrl = "/check-white.svg",
+  imgAlt = "check",
+}: {
+  caracteristicas?: string[];
+  imgUrl?: string;
+  imgAlt?: string;
+}) {
+  return (
+    <ul className="list-none pl-0 flex flex-col justify-center items-center gap-2">
+      {caracteristicas?.map((caracteristica, idx) => (
+        <motion.li
+          key={caracteristica}
+          className="flex items-center gap-2 mb-2 font-semibold text-md"
+          initial={{ opacity: 0, y: 30 }}
+          transition={{ duration: 0.5, delay: idx * 0.15 }}
+          viewport={{ once: true, amount: 0.6 }}
+          whileInView={{ opacity: 1, y: 0 }}
+        >
+          <Image
+            alt={imgAlt}
+            className="flex-shrink-0"
+            height={20}
+            src={imgUrl}
+            width={20}
+          />
+          <span>{caracteristica}</span>
+        </motion.li>
+      ))}
+    </ul>
+  );
+}
