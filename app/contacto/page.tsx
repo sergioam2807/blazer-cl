ab67d209932d3de20880e036c08d1957e5419a91
sergio <sergio.alvarez@evalueserve.com>
2025-05-24 23:02:35 -0400

    fix: error whit layout display

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -19,31 +19,31 @@ const contactos = [
   },
   {
     icon: "/icons/phone.svg",
-    alt: "Teléfono",
-    text: "Servicios: +56 9 3393 3641",
+    alt: "Teléfono Servicios",
+    text: "+56 9 3393 3641",
     href: "https://wa.me/56933933641",
   },
   {
     icon: "/icons/phone.svg",
-    alt: "Teléfono",
-    text: "Transporte: +56 9 7383 3905",
+    alt: "Teléfono Transporte",
+    text: "+56 9 7383 3905",
     href: "https://wa.me/56973833905",
   },
 ];
 
 export default function ContactoPage() {
   return (
-    <div className="w-full flex items-start text-start px-4 py-8 flex-col gap-8">
+    <div className="py-8">
       <HeaderPageTitle pageTitle="Contáctanos" />
 
       <div className="w-full flex flex-col sm:flex-row">
-        <div className="sm:w-1/3 max-w-3xl mx-auto relative z-10">
+        <div className="sm:w-1/3 max-w-3xl   mx-auto relative z-10">
           <div className="flex flex-col  gap-4 py-8 md:py-10">
             <p className="text-2xl font-extrabold leading-snug">
               Estamos aquí para ayudarte
             </p>
           </div>
-          <Card className="text-center py-8 px-8 bg-yellow-500/80">
+          <Card className="sm:text-start py-8 px-8 bg-yellow-500/80">
             <div className="absolute top-4 right-5 sm:right-3 sm:-top-2 h-64 w-0.5 bg-black ml-4" />
             <div className="absolute -top-5 right-2 sm:-right-3 sm:-top-10 w-72 h-0.5 bg-black mt-12" />
 
@@ -51,28 +51,39 @@ export default function ContactoPage() {
               Cotiza con nosotros
             </CardHeader>
             {contactos.map((item, idx) => (
-              <div key={idx} className="flex gap-4 items-center ml-3 mb-4">
-                <Image
-                  alt={item.alt}
-                  className="rounded-xl"
-                  height={30}
-                  src={item.icon}
-                  width={30}
-                />
-                {item.href ? (
-                  <a
-                    className="text-lg text-justify font-semibold  hover:text-gray-800 transition-colors"
-                    href={item.href}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    {item.text}
-                  </a>
-                ) : (
-                  <p className="text-lg text-justify font-semibold">
-                    {item.text}
-                  </p>
+              <div key={idx} className="ml-3 mb-4">
+                {/* Título arriba si corresponde */}
+                {(item.alt === "Teléfono Servicios" ||
+                  item.alt === "Teléfono Transporte") && (
+                  <span className="text-lg font-semibold mt-3 underline mr-8  ml-12 sm:text-start">
+                    {item.alt === "Teléfono Servicios"
+                      ? "Servicios"
+                      : "Transporte"}
+                  </span>
                 )}
+                <div className="flex gap-4 items-center">
+                  <Image
+                    alt={item.alt}
+                    className="rounded-xl"
+                    height={30}
+                    src={item.icon}
+                    width={30}
+                  />
+                  {item.href ? (
+                    <a
+                      className="text-lg text-justify font-semibold hover:text-gray-800 transition-colors"
+                      href={item.href}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >
+                      {item.text}
+                    </a>
+                  ) : (
+                    <p className="text-lg text-justify font-semibold">
+                      {item.text}
+                    </p>
+                  )}
+                </div>
               </div>
             ))}
             <CardFooter className="text-md font-light text-justify">
