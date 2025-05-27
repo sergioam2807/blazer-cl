91e998b2f16160f031bc0dcbe9c9a3695edd1f95
sergio <sergio.alvarez@evalueserve.com>
2025-05-26 22:43:29 -0400

    fix: update servicios page

--- app/servicios/page.tsx
+++ app/servicios/page.tsx
@@ -1,11 +1,11 @@
 import { serviciosData } from "../page";
 
+import AnimatedCard from "@/components/animatedCard";
 import HeaderPageTitle from "@/components/headerPageTitle";
-import CardComponent from "@/components/cardComponent";
 
 export default function ServiciosPage() {
   return (
-    <div className="w-full flex items-start text-start px-4 py-10 flex-col gap-8">
+    <div className="w-full flex items-center text-start px-4 py-10 flex-col gap-8">
       <HeaderPageTitle pageTitle="Servicios" />
       <div className="text-start flex flex-col gap-4 text-xl font-bold">
         <p className="text-2xl font-extrabold leading-snug">
@@ -16,12 +16,12 @@ export default function ServiciosPage() {
           ¿Tienes dudas? No dudes en contactarnos.
         </p>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
         {serviciosData.map((card, index) => (
-          <CardComponent
+          <AnimatedCard
             key={index}
-            icon={card.icon}
-            text={card.text}
+            img={card.icon}
+            subtitle={card.text}
             title={card.title}
             url={card.url}
           />
